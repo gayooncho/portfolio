a273d8911b7f325eeb44edcc7a55ad1ddd6166e4
GAYOON <100545291+gayooncho@users.noreply.github.com>
2022-05-27 15:29:41 +0900

    Update 데이터 상관관계 확인_01.r

--- project_1/R/데이터 상관관계 확인_01.r
+++ project_1/R/데이터 상관관계 확인_01.r
@@ -10,13 +10,24 @@ cor(df)
 library(ggcorrplot)
 ggcorrplot(cor(df))
 
-# Oneperson의 단위수가 크기 때문에 boxplot을 위해 제거
-df_oneD <- subset(df, select = -c(Oneperson))
-
 # 각각 데이터의 이상치 확인
 boxplot(df$Police)
 boxplot(df$CCTV)
 boxplot(df$Pub)
 boxplot(df$Oneperson)
 boxplot(df$Crime)
+
+# 이상치 데이터 -중구- 제거
+df_noju <- df[-24,]
+df_noju
+df_noju <- subset(df_noju, select = -c(X, District))
+
+# 피어슨 상관계수 확인
+cor(df_noju, method = "pearson")
+
+# 차트에 밀도 곡선, 상관성, 유의확률(별표)추가
+# 상관성, P값 정규분표 시각화? - 모수 검정 조건??
+install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+chart.Correlation(df_noju, histogram =, pch="+")
 
